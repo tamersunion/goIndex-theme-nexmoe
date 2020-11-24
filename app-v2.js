9bdb89fca63f7cbf8b718b13a9249d449fc3f46d
Hanada <33758411+HanadaLee@users.noreply.github.com>
2020-11-24 18:04:33 +0800

    Update app-v2.js

--- app-v2.js
+++ app-v2.js
@@ -7,29 +7,29 @@
  */
 // 在head 中 加载 必要静态
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/css/mdui.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/css/mdui.min.css">'
 );
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe@1.1.2/css/nexmoe.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/css/nexmoe.min.css">'
 );
 // markdown支持
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/markdown-it.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/js/markdown-it.min.js"></script>'
 );
 document.write(
-  "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-container{max-width:980px}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}</style>"
+  "<style>.mdui-appbar .mdui-toolbar{height:56px;font-size:1pc}.mdui-toolbar>*{padding:0 6px;margin:0 2px}.mdui-toolbar>i{opacity:.5}.mdui-toolbar>i{padding:0}.mdui-toolbar>a:hover,a.active,a.mdui-typo-headline{opacity:1}.mdui-list-item{transition:none}.mdui-list>.th{background-color:initial}.mdui-list-item>a{width:100%;line-height:3pc}.mdui-list-item{margin:2px 0;padding:0}.mdui-toolbar>a:last-child{opacity:1}</style>"
 );
 document.write(
-  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/css/DPlayer.min.css">'
+  '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/css/DPlayer.min.css">'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/DPlayer.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/js/DPlayer.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/hls.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/js/hls.min.js"></script>'
 );
 document.write(
-  '<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/flv.min.js"></script>'
+  '<script src="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/js/flv.min.js"></script>'
 );
 
 // 初始化页面，并载入必要资源
@@ -37,18 +37,6 @@ function init() {
   document.siteName = $("title").html();
   $("body").addClass("mdui-theme-primary-blue-grey mdui-theme-accent-blue");
   var html = `
-<header class="nexmoe-nav">
-    <div class="navSize">
-			<a href="/"><img class="avatar" src="https://i.loli.net/2020/04/24/wCxMZFtiv6aKmQ1.png"/></a>
-			<div class="nav_menu">
-				<ul class="menu_ul">
-					<li class="menu_li"><a href="https://5mayrain.github.io/" target="_blank">博客</a></li>
-					<li class="menu_li"><a href="https://drive.google.com/" target="_blank">登陆</a></li>
-				</ul>
-				<div class="nav_icon" ></div>
-			</div>
-	</div>
-</header>
 <div class="mdui-container">
   <div class="mdui-container-fluid">
     <div id="nav" class="mdui-toolbar nexmoe-item"> </div>
@@ -275,8 +263,8 @@ function file_code(path) {
 </div>
 <a href="${href}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 
-<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/ace.js"></script>
-<script src="//cdn.jsdelivr.net/gh/5MayRain/goIndex-theme-nexmoe/js/ext-language_tools.js"></script>
+<script src="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/js/ace.js"></script>
+<script src="//cdn.jsdelivr.net/gh/HanadaLee/goIndex-theme-nexmoe/js/ext-language_tools.js"></script>
 	`;
   $("#content").html(content);
 
@@ -516,4 +504,3 @@ $(function() {
 
   render(path);
 });
-//# sourceMappingURL=/sm/ca38655586335790b5a0312fa96fa55bbbf9628a01e85f152e7db6f937910a42.map
